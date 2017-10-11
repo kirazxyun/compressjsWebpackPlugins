541a9964b43114a8b10e6c2baff4760d8bca7845
zhangxiaoyun <zhangxiaoyun0828@sina.cn>
2017-10-11 15:51:03 +0800

    完成-超简单版

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # compressjsWebpackPlugins
-webpack js压缩插件
+webpack js压缩插件，做练习用 
+压缩算法使用uglify-es
---
+++ src/compressjs-webpack-plugin.js
@@ -0,0 +1,33 @@
+// 引入压缩算法uglifyjs，es版本才支持es6语法
+import UglifyJS from 'uglify-es';
+import RawSource from 'webpack-sources/lib/RawSource';
+
+class CompressjsWebpackPlugin {
+  constructor(options = {}) {
+    console.log(options);
+  }
+  // 在它的prototype上定义一个‘apply’方法
+  apply(compiler) {
+    // 指定挂载webpack事件钩子，
+    // emit是在发送资源到输出目录之前，在这里做文件的压缩
+    compiler.plugin('emit', (compilation, callback) => {
+      const assets = compilation.assets; // 获得资源
+      Object.keys(assets).forEach((file) => {
+        if(!(/.*\.js$/.test(file))) return;
+
+        const asset = assets[file];
+        let content = asset.source();
+        
+        const result = UglifyJS.minify(content);
+        if(result.error) return;
+
+        content = UglifyJS.minify(content);
+        assets[file] = new RawSource(result.code);
+      });
+      // 功能完成后调用webpack提供的回调
+      callback();
+    });
+  }
+}
+
+module.exports = CompressjsWebpackPlugin;  
